db9d474245642f439b6cb571fd0500dd0215d926
NekozillaXD <45684444+NekozillaXD@users.noreply.github.com>
2019-05-31 12:48:06 +0300

    Add files via upload

--- app/save.cpp
+++ app/save.cpp
@@ -1,20 +1,21 @@
 #include "functions.h"
+#include <QFileInfo>
 
-int saveresult(int score, QString name, QString file_name)
+bool saveresult(int score, QString name, QString file_name)
 {
     QFile file(file_name);
     // Если файл успешно открыт для записи в текстовом режиме
-    if(file.open(QIODevice::Append | QIODevice::Text))
+    if(file.exists() && file.open(QIODevice::Append | QIODevice::Text))
         {
             QTextStream writeStream(&file);
             writeStream << score <<" "<< name << endl;
             file.close();
-            return 0;
+            return true;
         }
     else
         {
             std::cout << "Can't open leaderboard" << std::endl;
-            return 1;
+            return false;
         }
 
 }
